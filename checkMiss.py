b18ba887d63c9147454d39b67ecc3a896a112551
rosaann <147429240@qq.com>
2019-04-28 16:45:42 +0800

    d

---
+++ checkMiss.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 28 09:17:37 2019
+
+@author: zl
+"""
+
+import os
+basePath = './images/'
+paths = os.listdir(basePath)
+print(paths)
