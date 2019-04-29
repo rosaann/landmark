1f06b5632644a2d0699165f34f23f71d18550af1
rosaann <147429240@qq.com>
2019-04-29 11:59:26 +0800

    d

--- down_others.py
+++ down_others.py
@@ -7,6 +7,10 @@ Created on Mon Apr 29 11:14:16 2019
 """
 
 from download import download_file
-#file = 'https://s3.amazonaws.com/google-landmark/metadata/train.csv'
-file = 'https://s3.amazonaws.com/google-landmark/metadata/train_attribution.csv'
-download_file(file, './train_attribution.csv')
+def down():
+    #file = 'https://s3.amazonaws.com/google-landmark/metadata/train.csv'
+    file = 'https://s3.amazonaws.com/google-landmark/metadata/train_attribution.csv'
+    download_file(file, './train_attribution.csv')
+    
+
+    
