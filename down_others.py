c9242d7480ded8b91b73b9e44ef4cdf01cf11d94
rosaann <147429240@qq.com>
2019-04-29 11:18:51 +0800

    d

---
+++ down_others.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 29 11:14:16 2019
+
+@author: zl
+"""
+
+from download import download_file
+download_file('https://s3.amazonaws.com/google-landmark/metadata/train.csv', './')
