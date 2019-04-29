c7d6af2f9242a4f224c78303a784e0be5f33e88d
rosaann <147429240@qq.com>
2019-04-29 11:26:00 +0800

    d

--- down_others.py
+++ down_others.py
@@ -7,4 +7,6 @@ Created on Mon Apr 29 11:14:16 2019
 """
 
 from download import download_file
-download_file('https://s3.amazonaws.com/google-landmark/metadata/train.csv', './train.csv')
+#file = 'https://s3.amazonaws.com/google-landmark/metadata/train.csv'
+file = 'https://s3.amazonaws.com/google-landmark/metadata/train_attribution.csv'
+download_file(file, './train_attribution.csv')
