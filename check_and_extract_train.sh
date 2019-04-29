6a2d907954cac5b77f28be92b6c83cd032619e36
rosaann <147429240@qq.com>
2019-04-29 12:01:40 +0800

    d

--- check_and_extract_train.sh
+++ check_and_extract_train.sh
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 # Number of processes to run in parallel.
-NUM_PROC=6
+NUM_PROC=1
 
 # Inclusive upper limit for file downloads.
 # Default of N=499 will download all files, i.e. images_000.tar...images_499.tar
