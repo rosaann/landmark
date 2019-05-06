db8e5a7681a25164715319d45c0ed3d9a4285c4b
rosaann <147429240@qq.com>
2019-05-06 10:36:59 +0800

    d

--- tools/count_10Group.py
+++ tools/count_10Group.py
@@ -11,9 +11,9 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 import random
+import ast
 
-
-def main():
+def genkeyGroups():
     data_dir = './data'
     df_train = pd.read_csv(os.path.join(data_dir, 'train.csv'))
     num = df_train.shape[0]
@@ -42,11 +42,23 @@ def main():
         key_group.append(key)
         if i % group_item_num == (group_item_num - 1) and (i / group_item_num) !=  group_num:
             key_group_list.append(key_group)
-            print('group ', i / group_item_num, ' len ', len(key_group))
+           # print('group ', i / group_item_num, ' len ', len(key_group))
     key_group_list[-1].extend(key_group)
-    print('group last', ' len ', len(key_group_list[-1]))
-            
-         
+  #  print('group last', ' len ', len(key_group_list[-1]))
+    fileObject = open(os.path.join(data_dir, 'key_groups.txt'), 'w')
+    for ip in key_group_list:
+        fileObject.write(ip)
+        fileObject.write('\n')
+    fileObject.close()
+def main():
+    genkeyGroups()
+    # key_group_list 每组group_item_num 个key
+    key_group_list = []
+    with open("file.txt", 'r') as f: 
+         key_group_list = ast.literal_eval(f.read())
+    for key_group in key_group_list:
+        print('key_group ', len(key_group))
+        
         
   #  fileObject = open('assign.txt', 'w')
   #  for t in landmark_id_list.items():
