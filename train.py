780e11b23d9b7feadd4dea8b28a6fcae28652b5d
rosaann <147429240@qq.com>
2019-05-07 13:35:41 +0800

    d

--- train.py
+++ train.py
@@ -90,7 +90,7 @@ def evaluate_single_epoch(config,gi, model, dataloader, criterion,
         log_dict['loss'] = sum(loss_list) / len(loss_list)
 
         if writer is not None:
-            for l in range(len(p)):
+            for l in range(len(p[0])):
                 f1 = utils.metrics.f1_score(labels[:,l], p[:,l], 'binary')
                 writer.add_scalar('val/f1_{:02d}'.format(l), f1, epoch)
 
