1ab92e96b2d2d84030bae2ebc343ee8f598151f4
rosaann <147429240@qq.com>
2019-05-07 11:29:27 +0800

    d

--- train.py
+++ train.py
@@ -138,8 +138,9 @@ def train_single_epoch(config, gi, model, dataloader, criterion, optimizer,
             optimizer.zero_grad()
 
        # predictions = np.argmax(probabilities.cpu().detach().numpy(), axis = 1)  
-        predictions , values= torch.max(probabilities, 1)
+        values, predictions = torch.argmax(probabilities, 1)
         print('predictions ', predictions.shape, predictions)
+        print('values ', values.shape, values)
        # predictions = predictions.tolist()
         accuracy = (predictions == labels).sum().float() / float(predictions.numel())
         log_dict['acc'] = accuracy.item()
